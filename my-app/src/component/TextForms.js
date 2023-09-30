6a64372f031517adc8c45ed02a50c5d69b203f74
adarsh8469 <123470349+adarsh8469@users.noreply.github.com>
2023-09-30 18:24:04 +0530

    Bug Fixed

--- my-app/src/component/TextForms.js
+++ my-app/src/component/TextForms.js
@@ -35,17 +35,16 @@ export default function TextForms(props) {
         <div className="mb-3" style={{fontSize : '60px'}}>
         <textarea className="form-control" value = {text} style = {{backgroundColor : props.mode === 'dark'?'#ffD':'white'}} onChange = {handleOnChange} id="mybox" rows="8"></textarea>
       </div>
-      <button className="btn btn-primary" onClick = {handleUpclick}>Convert to UpperCase</button>
-      <button className="btn btn-primary mx-3" onClick = {handleLoclick}>Convert to LowerCase</button>
-      <button className="btn btn-primary" onClick = {handletoBold}>Reverse the Alphabates</button>
-      <button className="btn btn-primary mx-3" onClick = {handleClear}>Clear</button>
+      <button disabled = {text.length === 0} className="btn btn-primary mx-1 my-1" onClick = {handleUpclick}>Convert to UpperCase</button>
+      <button disabled = {text.length === 0} className="btn btn-primary mx-1 my-1" onClick = {handleLoclick}>Convert to LowerCase</button>
+      <button disabled = {text.length === 0} className="btn btn-primary mx-1 my-1" onClick = {handletoBold}>Reverse the Alphabates</button>
+      <button disabled = {text.length === 0} className="btn btn-primary mx-1 my-1" onClick = {handleClear}>Clear</button>
       </div>
 
       <div className="container my-3">
           <h1>Your Text Summary</h1>
-          <p><b>{text.split(' ').length}</b> words and <b>{text.length}</b> characters</p>
-          <p>Average Reader will take <b>{0.008 * text.split(' ').length} Minute to read above words.</b></p>
-          <p>Average Reader will take <b>{0.0032 * text.split(' ').length} Minute to read above words.</b></p>
+          <p><b>{text.split(' ').filter((element) => { return element.length !== 0}).length}</b> words and <b>{text.length}</b> characters</p>
+          <p>Average Reader will take <b>{0.008 * text.split(' ').filter((element) => { return element.length !== 0}).length}</b> Minute to read above words.</p>
           <h2>Preview</h2>
           <p>{text.length > 0?text:"Enter Some Text in the Above TextField to Preview it Here!!!"}</p>
       </div>
